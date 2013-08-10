558bab23d808971638cc89ec63d5f0e3dc70c970
snorthway <stephanie.northway@students.olin.edu>
2013-08-09 22:52:46 -0400

    testing a photo

--- newPortfolio/views.py
+++ newPortfolio/views.py
@@ -1,6 +1,11 @@
 from newPortfolio import app
 from flask import render_template
+import os
+
+def aws_url(folder, photo):
+	return "https://s3.amazonaws.com/%s/%s/%s" % (os.environ.get('S3_BUCKET'), folder, photo)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	photo = aws_url("photography","IMG_6461e.jpg")
+	return render_template('home.html', photo=photo)
